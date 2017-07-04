d7a7affb416694639f8508121229ef4594e5144d
Alina Chin <alina.chin@gmail.com>
2017-07-04 16:40:27 -0700

    added manual backup item to main popup menu

--- app/src/main/java/ca/alina/to_dolist/MainActivity.java
+++ app/src/main/java/ca/alina/to_dolist/MainActivity.java
@@ -173,6 +173,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // TODO disable/hide "Backup" item based on settings
         return true;
     }
 
