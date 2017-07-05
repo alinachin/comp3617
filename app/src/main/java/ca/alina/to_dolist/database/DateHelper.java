c8cdbc623c602004b6d05816d7e67766609e6ccb
Alina Chin <alina.chin@gmail.com>
2017-07-04 19:46:50 -0700

    cleaned up DateHelper a bit

--- app/src/main/java/ca/alina/to_dolist/database/DateHelper.java
+++ app/src/main/java/ca/alina/to_dolist/database/DateHelper.java
@@ -12,13 +12,8 @@ import org.joda.time.DateTime;
 import org.joda.time.Duration;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
-import org.joda.time.Period;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import ca.alina.to_dolist.R;
 
 public final class DateHelper {
     public static Date now() {
@@ -26,27 +21,12 @@ public final class DateHelper {
     }
 
     public static Date autoStartTime() {
-//        final Date result;
-//
-//        final DateTime dt = new DateTime();
-//        final DateTime resultDt = dt.plusHours(1).withMinuteOfHour(0);
-//        result = resultDt.toDate();
         DateTime resultDt = new DateTime().plusHours(1).withMinuteOfHour(0);
-        return resultDt.toDate();
-    }
-
-    public static Date getBeginningOfDay(final Date day) {
-        DateTime resultDt = new DateTime(day).withTimeAtStartOfDay();
         return resultDt.toDate();
     }
 
     public static Date getBeginningOfDay(LocalDate day) {
         DateTime resultDt = day.toDateTimeAtStartOfDay();
-        return resultDt.toDate();
-    }
-
-    public static Date getEndOfDay(final Date day) {
-        DateTime resultDt = new DateTime(day).plusDays(1).withTimeAtStartOfDay().minusMillis(1);
         return resultDt.toDate();
     }
 
@@ -92,7 +72,7 @@ public final class DateHelper {
         // e.g. "Today" or "" if nothing fits
         String desc;
 
-        Duration duration = new Duration(date.toDateTimeAtCurrentTime(), DateTime.now());
+        Duration duration = new Duration(date.toDateTimeAtStartOfDay(), DateTime.now());
         if (Math.abs(duration.getStandardDays()) >= 7) {
             return "";
         }
