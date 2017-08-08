afdb51e9defbed7b38d4b606dc8097e8a888e407
Alina Chin <alina.chin@gmail.com>
2017-08-07 22:31:34 -0700

    added KV backup helper

---
+++ app/src/main/java/ca/alina/to_dolist/MyBackupHelper.java
@@ -0,0 +1,24 @@
+package ca.alina.to_dolist;
+
+import android.app.backup.BackupAgentHelper;
+import android.app.backup.SharedPreferencesBackupHelper;
+
+/**
+ * Created by Alina on 8/7/2017.
+ */
+
+public class MyBackupHelper extends BackupAgentHelper {
+    static final String myPrefs = "myprefs";
+
+    @Override
+    public void onCreate() {
+        String defaultPrefsName = getPackageName() + "_preferences";
+
+        SharedPreferencesBackupHelper backupHelper = new SharedPreferencesBackupHelper(
+                this,
+                defaultPrefsName,
+                StateMachine.STATE_PREF_FILE
+        );
+        addHelper(myPrefs, backupHelper);
+    }
+}
